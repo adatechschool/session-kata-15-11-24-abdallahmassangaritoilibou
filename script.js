bc7cc9b52bb3927daae1eda3fbf8d38c3308d20c
abdallahmassangaritoilibou <abdallah.toilbou@laposte.net>
2024-11-15 12:29:52 +0100

    ajout latin ToMors

--- script.js
+++ script.js
@@ -6,3 +6,41 @@ return text.split('');  //je converti les cheine de character
 const text = "Hello,World"
 const characters = getLatinCharacterList(text);
 console.log(characters);//j'affiche le mot Hello world dans la console en dans un tableau[]
+
+
+function translateLatinCharacter(){
+    const latinToMorse ={
+        'A':'.-',
+        'B':'-...',
+        'C':'-.-.',
+        'D':'-..',
+        'E':'.',
+        'F':'..-.',
+        'G':'--.',
+        'H':'....',
+        'I':'..',
+        'J':'.---',
+        'K':'-.-',
+        'L':'.-..',
+        'M':'--',
+        'N':'-.',
+        'O':'---',
+        'P':'.--.',
+        'Q':'--.-',
+        'R':'.-.',
+        'S':'...',
+        'T':'-',
+        'U':'..-',
+        'V':'...-',
+        'W':'.--',
+        'X':'-..-',
+        'Y':'-.--',
+        'Z':'--..'
+    }
+//     const upperCaseChar = characters.toUpperCase;
+//     return morseCode[upperCaseChar] || 'Caractère non trouvé';
+// }
+
+// const characterToTranslate ='A';
+// const morseEquivalent = translateLatinCharacter(characterToTranslate);
+// console.log(morseEquivalent)
